96c2523d0cbc12855321b645e5accfe2415c388e
乔鑫 <59754675+qiaoqiao-haiyang@users.noreply.github.com>
2020-05-26 12:38:11 +0800

    小例子更新

--- src/ProgramExample.java
+++ src/ProgramExample.java
@@ -33,13 +33,41 @@ public class ProgramExample {
         System.out.print("周数为："+ z + ",天数为：" + t);
     }
     @Test
-    public void testFn(){
-    int l = 54321;
-    int w = l/10000;
-    int q = l/1000%10;
-    int b = l/100%10;
-    int s = l/10%10;
-    int g = l%10;
-        System.out.println("五位数之和为：" +(w+q+b+s+g));
+    public void testFn() {
+//    int l = 54321;
+//    int w = l/10000;
+//    int q = l/1000%10;
+//    int b = l/100%10;
+//    int s = l/10%10;
+//    int g = l%10;
+//        System.out.println("五位数之和为：" +(w+q+b+s+g));
+        int a = 4;
+        int b = 6;
+        //逻辑与：＆同为１时为１，否则为０
+        if (a < 3 & b++ > 6) {
+            System.out.println(a + b);
+        } else {
+            System.out.println(a * b);
+        }
+        //短路与
+        //ｉｆ（ａ＝＝１＆＆ｂ＝＝２）　这是说既要满足ａ＝１也要满足ｂ＝２
+        if (a < 6 && b++ > 6) {
+            System.out.println(a - b);
+        } else {
+            System.out.println(a / b);
+        }
+        //逻辑或：｜同为０时为０，否则为１
+        if (a < 3 | b++ > 6) {
+            System.out.println(a + b);
+        } else {
+            System.out.println(a * b);
+        }
+        //短路或
+        //ｉｆ（ａ＝＝１｜｜ｂ＝＝２）　这是说或者满足ａ＝１或者要满足ｂ＝２
+        if (a < 3 || b++ > 6) {
+            System.out.println(a + b);
+        } else {
+            System.out.println(a * b);
+        }
     }
 }
