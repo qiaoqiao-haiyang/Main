10075334de5dd4e2856bcb7c489536e34b517375
乔鑫 <59754675+qiaoqiao-haiyang@users.noreply.github.com>
2020-05-26 14:02:58 +0800

    小例子更新

--- src/ProgramExample.java
+++ src/ProgramExample.java
@@ -21,19 +21,26 @@ public class ProgramExample {
      */
     /*
     *3,根据天数计算周数和剩余天数，天数通过键盘输入
-    *
-    *
-     */
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("请输入天数d=");
+    *   Scanner sc = new Scanner(System.in);
+        System.out.print("请输入天数d=");
         int d = sc.nextInt();
         int z = d/7;
         int t = d%7;
         System.out.print("周数为："+ z + ",天数为：" + t);
+     */
+    public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+        System.out.println("请输入第一个数：");
+        int yi = sc.nextInt();
+        System.out.println("请输入第二个数：");
+        int er = sc.nextInt();
+        int temp;
+        temp = yi;
+        yi=er;
+        er = temp;
+        System.out.println("第一个数"+yi +"第二个数"+er);
     }
-    @Test
-    public void testFn() {
+
 //    int l = 54321;
 //    int w = l/10000;
 //    int q = l/1000%10;
@@ -41,7 +48,7 @@ public class ProgramExample {
 //    int s = l/10%10;
 //    int g = l%10;
 //        System.out.println("五位数之和为：" +(w+q+b+s+g));
-        int a = 4;
+        /*int a = 4;
         int b = 6;
         //逻辑与：＆同为１时为１，否则为０
         if (a < 3 & b++ > 6) {
@@ -68,6 +75,98 @@ public class ProgramExample {
             System.out.println(a + b);
         } else {
             System.out.println(a * b);
+        }*/
+        /*
+        输入一个字符，判断是大写还是小写，输出提示信息
+              char  a = 'G';
+        if(a>='a' && a<='z'){
+            System.out.println("小写字母");
+        }else if(a>='A'&&a<='Z'){
+            System.out.println("大写字母");
+        }else{
+            System.out.println("其他字符");
         }
+         */
+    @Test
+        public void testFn() {
+        final int a = 1;//final修饰不可变量
+    switch (a){
+        case a:
+            System.out.println(1);
+            break;
+        case 2:
+                System.out.println(2);
+            break;
+        case 4:
+            System.out.println(3);
+            break;
+        default:
+            System.out.println("default");
+            break;
+    }
+    }
+    /**
+     * 输入一个月数，然后输入对应月份有多少天（不考虑闰年），将天数输出。
+     */
+    @Test
+    public void testFn8(){
+        int d = 1;
+        switch(d){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31天");
+                break;
+            case 2:
+                System.out.println("28天");
+            break;
+            default:
+                System.out.println("30天");
+        }
+        int a = 10, b = 12;
+        String ysf = "+";
+        switch(ysf){
+            case"+":
+                System.out.println(a+b);
+                break;
+        }
+    }
+    /**
+     * while循环
+     */
+    @Test
+    public void testFn9(){
+    int i = 10;
+    while(i-->4){
+        System.out.println(i);
+    }
+    }
+    /**
+     * 计算1-100之间所有整数的和。。5050
+     */
+    @Test
+    public void testFn10(){
+        int i = 1;
+        int sum = 0;
+        while (i<=100){
+            sum+=i++;
+        }
+        System.out.println(sum);
+    }
+    /**
+     * 计算10的阶乘
+     */
+    @Test
+    public void testFn11(){
+        int i = 1;
+        int mul = 1;
+        while (i<10){
+            mul *=i++;
+        }
+        System.out.println(mul);
     }
 }
